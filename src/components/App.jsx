516f59eed05725fcb449a76b4869951a940d71b9
CherednikNatalya <104647370+CherednikNatalya@users.noreply.github.com>
2023-03-19 15:15:15 +0200

    10

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,19 +1,16 @@
+import { Layout } from "components/Layout/Layout";
+import { AppBar } from "components/AppBar/AppBarp";
+import { TaskForm } from "components/TaskForm/TaskForm";
+import { TaskList } from "components/TaskList/TaskList";
+
 export const App = () => {
-  const appState ={
-    task:[
-      { id: 0, text: "Learn HTML and CSS", completed: true },
-      { id: 1, text: "Get good at JavaScript", completed: true },
-      { id: 2, text: "Master React", completed: false },
-      { id: 3, text: "Discover Redux", completed: false },
-      { id: 4, text: "Build amazing apps", completed: false },
-    ],
-    filters: {
-      status: 'all'
-    },
-  }
+
   return (
-    // ,,,
+    <Layout>
+    <AppBar />
+    <TaskForm />
+    <TaskList />
+  </Layout>
+);
 
-
-  );
 };
