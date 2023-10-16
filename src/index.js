f9ae8f7e710ce3253de0402c333582959be4753d
CherednikNatalya <104647370+CherednikNatalya@users.noreply.github.com>
2023-10-16 11:07:33 +0300

    radux

--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
-import React from 'react';
-import {BrowserRouter} from 'react-router-dom'
-import ReactDOM from 'react-dom/client';
-import { App } from 'components/App';
-import './index.css';
+import ReactDOM from "react-dom/client";
+import { App } from "components/App";
+import { Provider } from "react-redux";
+import {store} from './redux/store'
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter basename="/Redux">
-    <React.StrictMode>
-    <App />
-  </React.StrictMode>
-  </BrowserRouter>
+ReactDOM.createRoot(document.getElementById("root")).render(
+<Provider store={store}>
+<App />
+</Provider>
 );
