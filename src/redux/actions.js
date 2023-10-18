86811b35d91a1a959016bd393521c646e288a7ed
CherednikNatalya <104647370+CherednikNatalya@users.noreply.github.com>
2023-10-18 11:40:45 +0300

    add reduser, store

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,33 +1,33 @@
 import { nanoid } from "nanoid";
 
-const addTask = text => {
-    return{
-    type:'tasks/addTasks',
-    payload:{
-        id: nanoid(),
-        text,
-        completed: false,
-    }
-    }
+export const addTask = text => {
+  return {
+    type: "tasks/addTask",
+    payload: {
+      id: nanoid(),
+      completed: false,
+      text,
+    },
+  };
 };
 
-const deleteTask =taskId => {
-    return{
-    type:'tasks/deleteTasks',
+export const deleteTask = taskId => {
+  return {
+    type: "tasks/deleteTask",
     payload: taskId,
-    }
+  };
 };
 
-const toggleCompleted = taskId =>{
-   return{
-    type:'tasks/toggleCompleted',
+export const toggleCompleted = taskId => {
+  return {
+    type: "tasks/toggleCompleted",
     payload: taskId,
-   }
+  };
 };
 
-const setStatusFilter = value => {
-   return{
+export const setStatusFilter = value => {
+  return {
     type: "filters/setStatusFilter",
     payload: value,
-   }
+  };
 };
